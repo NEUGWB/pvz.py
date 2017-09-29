016b2ca075f667c7f498180987aaf476fb463087
lmintlcx <lmintlcx@outlook.com>
2017-09-29 14:22:53 +0800

    细节微调

--- pvz.py
+++ pvz.py
@@ -4,7 +4,8 @@ description: a python module contains some method of game "Plants Vs Zombies"
 author: no_doudle
 date: 2017-2-24 20:00
 '''
-import win32api, win32con, win32gui, win32process, ctypes 
+
+import win32api, win32con, win32gui, win32process, ctypes, threading
 from time import sleep
 
 x, y = win32api.GetCursorPos()
@@ -25,14 +26,14 @@ bytes_read = ctypes.c_size_t()
 def ReadMemory(address):
     global PROCESS, buffer
     rPM(PROCESS.handle, address, buffer, 4, ctypes.byref(bytes_read))
-    ret=0x0
+    ret = 0x0
     cnt = len(buffer.value) -1
     while cnt >= 0:
         ret *= 256
         ret += buffer.value[cnt]
         cnt -= 1
     return ret
-    
+
 def MUP(x, y, right = False):
     global Screenx,Screeny
     x *= (Screenx/800)
@@ -72,47 +73,44 @@ def MoveClick(x, y, right = False):
     sleep(0.01)
     win32api.SetCursorPos((x_0, y_0))
 
-def safeClick():
+def SafeClick():
     Click(60, 50, True)
-    
+
 def Countdown():
     global memoryAddress
     if memoryAddress < 0 :
         memoryAddress = ReadMemory(0x6a9ec0) + 0x768
         memoryAddress = ReadMemory(memoryAddress) + 0x559c
-        
     countdown = ReadMemory(memoryAddress) 
     while countdown == 0 :
         countdown = ReadMemory(memoryAddress)
     return countdown
-    
+
 def ChooseCard(row, column, imitater = False):
     if(imitater):
         MoveClick(490, 550)
         sleep(0.2)
-        x, y = 190, 125
+        x = 190 + 50/2 + (column - 1) * 51
+        y = 125 + 70/2 + (row - 1) * 71
     else:
-        x, y = 22, 123
-    x += 50/2
-    y += 70/2
-    x += (column - 1)* 53
-    y += (row - 1)* 70
+        x = 22 + 50/2 + (column - 1) * 53
+        y = 123 + 70/2 + (row - 1) * 70
     Click(x, y)
-        
+    sleep(0.2)
+
 def LetsRock():
-    sleep(0.01)
-    MDOWN(225, 565)
-    sleep(0.01) 
-    MUP(225, 565) 
+    sleep(0.2)
+    MDOWN(234, 567)
+    sleep(0.1)
+    MUP(234, 567)
 
 def Card(num):
-    Click(50 + 50 * num, 70)
-    
+    Click(50 + 51 * num, 42)
+
 def Pnt(pnt):
     global scene
     row = pnt[0]
     column = pnt[1]
-    
     if (scene == 'DE') or (scene == 'NE'):
         Click(80 * column, 30 + 100 * row)
     elif (scene == 'PE') or (scene == 'FE'):
@@ -126,20 +124,19 @@ def Pnt(pnt):
 def preJudge(t, hugewave = False):
     if(hugewave == False):
         while(Countdown() > t):
-            pass 
-    
+            pass
     else:
         while(Countdown() > 4):
             pass
         sleep((724-t)/100)
 
 def Pao(row, column):
-    global nowPao,paoList
+    global nowPao, paoList
     nowPao %= len(paoList)
-    for i in range(10):
+    for i in range(3):
         Pnt(paoList[nowPao])
     Pnt((row, column))
-    safeClick()
+    SafeClick()
     nowPao += 1
     
 def wave20():
